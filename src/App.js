a4eaca6ba3dcf78ef40f8a9e965ea7380e822878
root <root@DESKTOP-IONV53U.localdomain>
2022-02-16 18:43:37 +0800

    Loginpage component complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import React from 'react';
 import './App.css';
+import LoginPage from './components/Loginpage/Loginpage';
+import {useState} from 'react';
 
-function App() {
+function App () {
+  const [isLoggedIn, setIsLoggedIn] = useState (false);
+
   return (
     <div className="App">
-
+      <LoginPage isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
     </div>
   );
 }
