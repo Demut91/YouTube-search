e54369aafce599744f99b26b3209c6e884f0f769
root <root@DESKTOP-IONV53U.localdomain>
2022-02-23 22:51:52 +0800

    refactoring

--- src/components/Mainpage/Maipage.jsx
+++ src/components/Mainpage/Maipage.jsx
@@ -1,35 +1,24 @@
-import React from "react";
+import { useState, React } from "react";
 import axios from "axios";
 import Header from "../Header/Header";
 import Searching from "../Searching/Searching";
 import { Routes, Route } from "react-router-dom";
 import Favorites from "../Favorites/Favorites";
 import SearchingResults from "../SearchingResults/SearchingResults";
-import { useNavigate}  from "react-router";
+import { useNavigate } from "react-router";
 
-function Mainpage({
-  queries,
-  setQueries,
-  modal,
-  showModal,
-  setIsLoggedIn,
-  login,
-  setTotalResults,  
-  setInputValue,
-  inputValue,
-  videos, 
-  totalResults,
-  setVideos,
-  KEY
-}) {
-
+function Mainpage({ queries, setQueries, setIsLoggedIn, login }) {
+  const [modal, showModal] = useState(false);
+  const [videos, setVideos] = useState([]);
+  const [totalResults, setTotalResults] = useState(null);
+  const [inputValue, setInputValue] = useState("");
   const navigate = useNavigate();
 
+  const KEY = "AIzaSyD7Y0TCa1s867DB51jEhNv20ljCQYXFKG4";
 
   function handleChange({ target: { value } }) {
     setInputValue(value);
   }
-
 
   function savingQuery(values) {
     let arr = [];
@@ -45,9 +34,11 @@ function Mainpage({
   async function search(word) {
     if (word.length !== 0) {
       try {
-        const res = await axios.get(
-          `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=12&q=${word}&type=video&key=${KEY}`
-        ).then(navigate("/main/results"))       
+        const res = await axios
+          .get(
+            `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=12&q=${word}&type=video&key=${KEY}`
+          )
+          .then(navigate("/main/results"));
         setVideos(res.data.items);
         setTotalResults(res.data.pageInfo.totalResults);
       } catch (err) {
@@ -56,35 +47,13 @@ function Mainpage({
     }
   }
 
-
   return (
     <>
-      {/* <button
-          onClick={() => {
-            console.log(queries);
-            
-          }}
-        ></button> */}
       <Header quit={quit} />
       <Routes>
         <Route
           path="/searching"
-          element={
-            <Searching
-              savingQuery={savingQuery}
-              modal={modal}
-              showModal={showModal}
-              setTotalResults={setTotalResults}              
-              setInputValue={setInputValue}
-              inputValue={inputValue}
-              videos={videos}              
-              totalResults={totalResults}
-              setVideos={setVideos}
-              KEY={KEY}
-              search={search}
-              handleChange={handleChange}
-            />
-          }
+          element={<Searching search={search} handleChange={handleChange} />}
         />
         <Route
           path="/favorites"
@@ -95,11 +64,9 @@ function Mainpage({
               modal={modal}
               showModal={showModal}
               KEY={KEY}
-              setTotalResults={setTotalResults}             
+              setTotalResults={setTotalResults}
               setInputValue={setInputValue}
               inputValue={inputValue}
-              videos={videos}             
-              totalResults={totalResults}
               setVideos={setVideos}
             />
           }
@@ -108,15 +75,15 @@ function Mainpage({
           path="/results"
           element={
             <SearchingResults
-            savingQuery={savingQuery}
-            videos={videos}
-            search={search}
-            totalResults={totalResults}
-            inputValue={inputValue}
-            handleChange={handleChange}
-            modal={modal}
-            showModal={showModal}
-          />
+              savingQuery={savingQuery}
+              videos={videos}
+              search={search}
+              totalResults={totalResults}
+              inputValue={inputValue}
+              handleChange={handleChange}
+              modal={modal}
+              showModal={showModal}
+            />
           }
         />
       </Routes>
--- src/components/Searching/Searching.jsx
+++ src/components/Searching/Searching.jsx
@@ -3,12 +3,7 @@ import "./Searching.css";
 import { Input } from "antd";
 const { Search } = Input;
 
-function Searching({
-  handleChange,
-  search
-}) {
-
-
+function Searching({ handleChange, search }) {
   return (
     <div className="searching">
       <h1 className="searching__heading">Поиск видео</h1>
